46bf4987693c83e72adcb657c1ef188316c76975
ptoner <ptoner@pm.me>
2023-06-07 14:59:21 -0400

    Testing out Github action

--- getcid/index.js
+++ getcid/index.js
@@ -5,4 +5,6 @@ let args = process.argv.slice(2);
 
 const v1 = CID.parse(args[0]);
 
-fs.writeFileSync("dist/ipfs.json", JSON.stringify({ cid: v1.toV0().toString()}));
+fs.writeFileSync("../ipfs.json", JSON.stringify({ cid: v1.toV0().toString()}));
+
+console.log("Successfully updated ipfs.json")
